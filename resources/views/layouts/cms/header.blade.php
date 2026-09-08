42d830b0242df2f6cf3d1a21fe6b06270ba62d42
IqbalRadjasa <iqbal.radjasa@gmail.com>
2026-09-08 08:59:20 +0700

    refactor: header cms

--- resources/views/layouts/cms/header.blade.php
+++ resources/views/layouts/cms/header.blade.php
@@ -23,14 +23,14 @@
                     </svg>
 
                     <div class="hidden sm:flex flex-col items-start mx-3">
-                        <span class="text-sm font-semibold text-primary-light">
+                        {{-- <span class="text-sm font-semibold text-primary-light">
                             Admin
-                        </span>
-                        {{-- @auth
+                        </span> --}}
+                        @auth
                             <span class="text-sm font-semibold text-primary-light">
                                 {{ Auth::user()->name }}
                             </span>
-                        @endauth --}}
+                        @endauth
                     </div>
                     <span class="w-8 h-8 rounded-full bg-red-200 flex items-center justify-center text-red-500">
                         <i class="ri-user-fill"></i>
